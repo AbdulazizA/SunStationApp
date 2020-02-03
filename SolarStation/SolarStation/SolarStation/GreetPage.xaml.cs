bb06723815cf0afcd1a55ba930a481a4243026a6
Abdulaziz Almoallim <aziz.almoallim@gmail.com>
2020-02-03 15:01:57 -0500

    First Set up of greet page

---
+++ SolarStation/SolarStation/SolarStation/GreetPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SolarStation
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            // Below it similar to GreetPage.xaml label
+            //Content = new Label
+            //{
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    Text = "enerfi",
+            //};
+        }
+    }
+}
